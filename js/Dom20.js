e9b746a175299f4feba0cb89a0ff5f687600cba1
leonelestrada25 <estradabarreraleonelarnulfo@gmail.com>
2026-04-07 13:01:12 -0600

    Se agrego la cuenta Regresiva Para el mundial

--- js/Dom20.js
+++ js/Dom20.js
@@ -1,13 +1,14 @@
 import hamburgerMenu from "./menu_amburguesa.js";
 import { digitalclock, alarm } from "./reloj.js";
-import { moveBall, shortcut } from "./teclado.js"
+import { moveBall, shortcut } from "./teclado.js";
+import countdown from "./cuentaRegresiva.js"
 //Menu de navegacion de boton amburguesa.
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) =>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalclock("#reloj","#activar-reloj","#desactivar-reloj")
     alarm("assets/ringtones-super-mario-bros.mp3","#activar-alarma","#desactivar-alarma")
-
+    countdown("countdown", "Jun 11, 2026 13:00:00","Día del mundial");
 });
 //Reloj digital.
 d.addEventListener("keydown", (e) => {
